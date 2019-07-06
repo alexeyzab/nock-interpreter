ceb6661f2e5aa91072afed0d02e722c2cf526a9b
Alexey Zabelin <hello@alexeyzabelin.com>
2019-07-05 23:52:18 -0700

    Prepare to parse

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![feature(box_syntax, box_patterns)]
 
+extern crate nom;
+use nom::{IResult};
+
 use std::fmt;
 use Noun::*;
 
@@ -41,6 +44,20 @@ pub enum Expr {
 pub struct Error(Noun);
 
 pub type Possibly<T> = Result<T, Error>;
+
+pub fn eval(expr: Expr) -> Possibly<Noun> {
+    match expr {
+        Expr::Noun(n) => Ok(n),
+        Expr::Wut(n) => wut(n),
+        Expr::Lus(n) => lus(n),
+        Expr::Tis(n) => tis(n),
+        Expr::Net(n) => net(n),
+        Expr::Hax(n) => hax(n),
+        Expr::Tar(n) => tar(n),
+    }
+}
+
+
 
 // `?`
 // ?[a b]              0
@@ -140,6 +157,25 @@ pub fn hax(input: Noun) -> Possibly<Noun> {
 }
 
 // `*`
+// *[a [b c] d]        [*[a b c] *[a d]]
+
+// *[a 0 b]            /[b a]
+// *[a 1 b]            b
+// *[a 2 b c]          *[*[a b] *[a c]]
+// *[a 3 b]            ?*[a b]
+// *[a 4 b]            +*[a b]
+// *[a 5 b c]          =[*[a b] *[a c]]
+
+// *[a 6 b c d]        *[a *[[c d] 0 *[[2 3] 0 *[a 4 4 b]]]]
+// *[a 7 b c]          *[*[a b] c]
+// *[a 8 b c]          *[[*[a b] a] c]
+// *[a 9 b c]          *[*[a c] 2 [0 1] 0 b]
+// *[a 10 [b c] d]     #[b *[a c] *[a d]]
+
+// *[a 11 [b c] d]     *[[*[a c] *[a d]] 0 3]
+// *[a 11 b c]         *[a c]
+
+// *a                  *a
 pub fn tar(input: Noun) -> Possibly<Noun> {
     match input {
         Atom(_) => Err(Error(input)),
